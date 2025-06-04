46e2297cffc09e54a5ef660eae25c9ab0d1fc0d6
ChristianCoba <danielcoba2820@gmail.com>
2025-06-04 10:57:05 -0500

    Se cambio el path en el app.js

--- backend/app.js
+++ backend/app.js
@@ -51,3 +51,6 @@ app.use('/archivos', express.static('certificados'));
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
